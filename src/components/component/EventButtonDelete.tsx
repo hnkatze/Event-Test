7b629da8aac5f86156b83467764fc93c6abe6f40
Camilo Henriquez <henriquezhector1@hotmail.com>
2024-06-12 17:52:50 -0600

    Improve EventButtonDelete component

--- src/components/component/EventButtonDelete.tsx
+++ src/components/component/EventButtonDelete.tsx
@@ -1,11 +1,11 @@
 import { handleDeleteEvent } from "@/accions/serverActions";
 import { Button } from "../ui/button";
 
-export function EventButtonDelete({ id }: { id: string }) {
+export function EventButtonDelete({ id }: { readonly id: string }) {
   return (
     <form action={handleDeleteEvent}>
-      <input type="hidden" name="id" value={id} />
-      <Button type="submit" className="btn btn-red bg-red-500 rounded">
+      <input type='hidden' name='id' value={id} />
+      <Button type='submit' className='btn btn-red bg-red-500 rounded'>
         Delete
       </Button>
     </form>
