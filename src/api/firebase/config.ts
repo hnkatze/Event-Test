bdfe0ca8efb7a5fbb7d5a810be5424e2fce46f94
Camilo Henriquez <henriquezhector1@hotmail.com>
2024-06-12 17:52:02 -0600

    Update Firebase configuration in config.ts

--- src/api/firebase/config.ts
+++ src/api/firebase/config.ts
@@ -1,6 +1,5 @@
 import { initializeApp, FirebaseApp } from "firebase/app";
-import { getFirestore, Firestore } from 'firebase/firestore';
-
+import { getFirestore, Firestore } from "firebase/firestore";
 
 // Define la interfaz para la configuración de Firebase
 interface FirebaseConfig {
@@ -14,12 +13,12 @@ interface FirebaseConfig {
 
 // Configuración de Firebase usando variables de entorno para mayor seguridad
 const firebaseConfig: FirebaseConfig = {
-  apiKey: process.env.API_KEY ?? "defaultValue",
-  authDomain: process.env.AUTH_DOMAIN ?? "DOMAIN",
-  projectId: process.env.PROJECT_ID ?? "projectID",
-  storageBucket: process.env.STORAGE ?? " storageBucket ",
-  messagingSenderId: process.env.MESSA ?? " messagingSenderId ",
-  appId: process.env.APP_ID ?? " appId"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? "",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ?? "",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? "",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ?? "",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ?? "",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ?? "",
 };
 
 // Inicializa Firebase App
