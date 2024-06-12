be75cccd7d89b3cd77e2c2e3c82ccefbb50521f9
Camilo Henriquez <henriquezhector1@hotmail.com>
2024-06-12 17:52:50 -0600

    Update utility functions in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+//this function will transform a launch object to a MyEvent object
+export function transformLaunchToMyEvent(launch: Launch): MyEvent {
+  return {
+    id: launch.flight_number.toString(),
+    name: launch.mission_name,
+    description: launch.details ?? "No disponible",
+    date_start: launch.launch_date_utc,
+    date_end: launch.launch_date_utc,
+    location: launch.launch_site?.site_name_long ?? "Desconocido",
+    organizer: "Elonk Musk",
+    contact_info: "ElonMusk@spacex.com",
+    capacity: 6,
+  };
 }
